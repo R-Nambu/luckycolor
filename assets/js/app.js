a6d922ca4ea71293140db9dc7c4b4dd3a7532f0f
南部遼 <>
2019-12-28 15:46:07 +0800

    色追加

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,8 +5,8 @@ let result = document.getElementById('result');
 let bg = document.getElementsByClassName('result')[0];
 
 function luckycolor(){
-    let res = ['赤', '青', '黑', '白', '紫', '緑'];
-        let backgroundColor = ['red', 'blue', 'black', 'white', 'purple', 'green'];
+    let res = ['赤', '青', '黑', '白', '紫', '緑', 'ピンク', '黄', '水色', '紺', 'オレンジ'];
+        let backgroundColor = ['red', 'blue', 'black', 'white', 'purple', 'green', 'pink', 'yellow', 'skyblue', 'darkblue', 'orange'];
         let randomNum = Math.random();
         let num = Math.floor(randomNum * res.length);
 
@@ -14,7 +14,7 @@ function luckycolor(){
         
         bg.style.backgroundColor = backgroundColor[num];
 
-        if (num == 3){
+        if (num == 3 || num == 6 || num == 7 || num == 8){
             result.style.color = 'black';
         } else {
             result.style.color = 'white';
